414c4204b0438ee44dfcfdbdfabd809f7f05fb8c
Luana Carolina <luanalcj1@hotmail.com>
2022-12-09 11:26:28 -0300

    create-controller-eventos

---
+++ Backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+       
+        public EventoController()
+        {
+            
+        }
+        public IEnumerable<Evento> _eventos = new Evento[]{
+            new Evento(){
+                EventoId =1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QtdPessoas =250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemUrl ="foto.png"
+            },
+             new Evento(){
+                EventoId =2,
+                Tema = "Angular 13 e .NET 6",
+                Local = "São Paulo",
+                Lote = "1º Lote",
+                QtdPessoas =350,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemUrl ="perfil.png"
+            }
+        };
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _eventos;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        { 
+            return _eventos.Where(evento => evento.EventoId==id); 
+        }
+        
+    }
+}
