9110a0a7cd12cc660f507890f1a6a30f2e2887ef
Luana Carolina <luanalcj1@hotmail.com>
2023-01-31 21:51:05 -0300

    configs

--- FrontEnd/ProEventos/src/app/app.module.ts
+++ FrontEnd/ProEventos/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventosComponent
+    EventosComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
     HttpClientModule
   ],
   providers: [],
